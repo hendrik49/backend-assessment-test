c194da6cd921116b42d006fc49df4ba1123d3621
Hendrik <drikdoank@gmail.com>
2024-08-23 11:32:24 +0700

    create load factoru

--- database/factories/LoanFactory.php
+++ database/factories/LoanFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Loan;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class LoanFactory extends Factory
@@ -22,7 +23,15 @@ class LoanFactory extends Factory
     public function definition(): array
     {
         return [
-            // TODO: Complete factory
+            'user_id' => User::factory(), // Creates a new user or use an existing user ID
+            'amount' => $this->faker->numberBetween(1000, 10000),
+            'terms' => json_encode([
+                'monthly' => $this->faker->numberBetween(100, 500)
+            ]),
+            'outstanding_amount' => $this->faker->numberBetween(1000, 10000),
+            'currency_code' => $this->faker->currencyCode,
+            'processed_at' => $this->faker->optional()->dateTimeBetween('-1 year', 'now'),
+            'status' => $this->faker->randomElement(['due', 'repaid']),
         ];
     }
 }
